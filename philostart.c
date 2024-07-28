5c1a1d763ef071ce75080db3e71fef60e4dd7971
mboumlik <mohamedboumlik000@gmail.com>
2024-07-28 17:39:52 +0100

    ss

--- philostart.c
+++ philostart.c
@@ -6,7 +6,7 @@
 /*   By: mboumlik <mboumlik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 11:01:08 by mboumlik          #+#    #+#             */
-/*   Updated: 2024/07/28 11:51:27 by mboumlik         ###   ########.fr       */
+/*   Updated: 2024/07/28 17:29:20 by mboumlik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,29 @@ int init_data(t_data *data,int ac,char **av)
     data->meals = -1;
     if (ac == 6)
         data->meals = ft_atoi(av[5]);
-    
+    pthread_mutex_init(&data->time_to_eat,NULL);
+    pthread_mutex_init(&data->time_to_sleep,NULL);
+    pthread_mutex_init(&data->time_to_die,NULL);
+    pthread_mutex_init(&data->nb_philo,NULL);
+    return malloc_data(data);
 }
 int malloc_data(t_data *data)
 {
     data->philo = malloc(sizeof(t_philo) * data->nb_philo);
-    if (!data->philo)
-        return NULL;
-    
-    
+    if (!(data->philo))
+        return 2;
+    data->forks = malloc(sizeof(pthread_mutex_t) * data->nb_philo);
+    if (!(data->forks))
+        return (free(data->philo) ,2);
+    data->philotread = malloc(sizeof(pthread_t) * data->nb_philo);
+    if (!(data->philotread))
+        return (free(data->philo) ,free(data->forks) ,2);
+    return 0;
 }
 int philosophers(int ac,char **av)
 {
     t_data *data;
-    if (init_data(&data,ac,av) == 1)
-    {
+    if (init_data(&data,ac,av) != 0)
         return 2;
-    }
     
 }
